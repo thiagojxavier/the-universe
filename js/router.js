478a26b74b3124e63a952bb72bc67710f4693bc7
Thiago de Jesus Xavier <124896693+thiagojxavier@users.noreply.github.com>
2024-05-25 12:21:28 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -6,17 +6,22 @@ export class Router {
     }
 
     handle() {
-        const { pathname } = window.location;
+        let { pathname } = window.location;
 
         const root = document.querySelector("#root");
     
         this.changeStrong(pathname)
+
+        if(pathname === "/index.html") {
+            pathname = "/"
+        }
     
         const routeRequest = this.routes[pathname] || this.routes[404];
 
         const getLoading = this.loading();
 
         root.innerHTML = getLoading;
+        this.changeBackground(pathname);
     
         fetch(routeRequest)
             .then(data => data.text())
@@ -25,7 +30,6 @@ export class Router {
                 const discardingUnnecessaryContent = html.slice(index);
                 const indexEnd = discardingUnnecessaryContent.lastIndexOf("</div");
                 root.innerHTML = discardingUnnecessaryContent.slice(0, indexEnd);
-                console.log(discardingUnnecessaryContent.slice(0, indexEnd))
             })
     }
 
@@ -35,6 +39,38 @@ export class Router {
         }
 
         document.querySelector('.page-current').classList.remove("page-current");
+    
+        document.querySelector('a[href="' + pathname + '"]').classList.add("page-current");
+    }
+
+    loading() {
+        const img = `<div class="container-loading"><img src="./loading/rotate.png" alt="loading" class="loading"></div>`;
+
+        return img;
+    }
+
+    changeBackground(pathname) {
+        const body = document.querySelector('body');
+
+        switch (pathname) {
+            case "/":
+                body.className = "";
+                body.classList.add("home");
+                break;
+            case "/universe":
+                body.className = "";
+                body.classList.add("universe");
+                break;
+            case "/exploration":
+                body.className = "";
+                body.classList.add("exploration")
+                break;
+            default:
+                body.className = "";
+                body.classList.add("home");
+        }
+    }
+}
     
         document.querySelector('a[href="' + pathname + '"]').classList.add("page-current");
     }
